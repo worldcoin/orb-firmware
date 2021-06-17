96d9d83fd2d73aba11c3017303691df2a3c14a18
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2021-06-17 20:42:34 +0200

    Added compilation from CMake

---
+++ utils/utils.cmake
@@ -0,0 +1,36 @@
+# Get all subdirectories under ${current_dir} and store them
+# in ${result} variable
+macro(subdirlist result current_dir)
+    file(GLOB children ${current_dir}/*)
+    set(dirlist "")
+
+    foreach(child ${children})
+        if (IS_DIRECTORY ${child})
+            list(APPEND dirlist ${child})
+        endif()
+    endforeach()
+
+    set(${result} ${dirlist})
+endmacro()
+
+# Prepend ${CMAKE_CURRENT_SOURCE_DIR} to a ${directory} name
+# and save it in PARENT_SCOPE ${variable}
+macro(prepend_cur_dir variable directory)
+    set(${variable} ${CMAKE_CURRENT_SOURCE_DIR}/${directory})
+endmacro()
+
+# Add custom command to print firmware size in Berkley format
+function(firmware_size target)
+    add_custom_command(TARGET ${target} POST_BUILD
+            COMMAND ${CMAKE_SIZE_UTIL} -B
+            "${CMAKE_CURRENT_BINARY_DIR}/${target}${CMAKE_EXECUTABLE_SUFFIX}"
+            )
+endfunction()
+
+# Add a command to generate firmare in a provided format
+function(generate_object target suffix type)
+    add_custom_command(TARGET ${target} POST_BUILD
+            COMMAND ${CMAKE_OBJCOPY} -O${type}
+            "${CMAKE_CURRENT_BINARY_DIR}/${target}${CMAKE_EXECUTABLE_SUFFIX}" "${CMAKE_CURRENT_BINARY_DIR}/${target}${suffix}"
+            )
+endfunction()
