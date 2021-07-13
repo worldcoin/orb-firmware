371d4227e55ba3bc8a78c5e5474edaa9c3651a01
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2021-07-13 18:59:12 +0200

    Can now change compiler path before calling cmake

--- utils/cmake/arm-gcc-toolchain.cmake
+++ utils/cmake/arm-gcc-toolchain.cmake
@@ -1,7 +1,9 @@
-# This file must provide: full path to the compiler
-# using variables:
-# CMAKE_C_COMPILER
-# CMAKE_CXX_COMPILER (CMake will guess that variable based on CMAKE_C_COMPILER)
+# This file must provide: full path to the compiler by defining variables:
+# - CMAKE_C_COMPILER
+# - CMAKE_CXX_COMPILER
+#
+# The script can be called with `-DARM_TOOLCHAIN_DIR=path/to/compiler/bin`
+# to force the usage of a compiler from within the directory
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
@@ -11,19 +13,27 @@ set(TOOLCHAIN_PREFIX arm-none-eabi-)
 # Without that flag CMake is not able to pass test compilation check
 set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
 
-# Get full path to compiler
-execute_process(
-        COMMAND which ${TOOLCHAIN_PREFIX}gcc
-        OUTPUT_VARIABLE CMAKE_C_COMPILER
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if(NOT ARM_TOOLCHAIN_DIR)
+    # Get full path to compiler from current environment
+    execute_process(
+            COMMAND which ${TOOLCHAIN_PREFIX}gcc
+            OUTPUT_VARIABLE CMAKE_C_COMPILER
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
 
-# Get compiler directory
-execute_process(
-        COMMAND dirname ${CMAKE_C_COMPILER}
-        OUTPUT_VARIABLE ARM_TOOLCHAIN_DIR
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+    # Get compiler directory
+    execute_process(
+            COMMAND dirname ${CMAKE_C_COMPILER}
+            OUTPUT_VARIABLE ARM_TOOLCHAIN_DIR
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+else()
+    # Use ARM_TOOLCHAIN_DIR to get the compiler path
+    set(CMAKE_C_COMPILER ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}gcc)
+    set(CMAKE_CXX_COMPILER ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}g++)
+endif()
+
+message(STATUS "Compiler: ${CMAKE_C_COMPILER}")
 
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 
