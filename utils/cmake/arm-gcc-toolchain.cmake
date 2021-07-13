ec11671cb33d7e86671cb9bca1f6a6313c819728
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2021-07-13 17:15:39 +0200

    Better CMake files

--- utils/cmake/arm-gcc-toolchain.cmake
+++ utils/cmake/arm-gcc-toolchain.cmake
@@ -1,3 +1,8 @@
+# This file must provide: full path to the compiler
+# using variables:
+# CMAKE_C_COMPILER
+# CMAKE_CXX_COMPILER (CMake will guess that variable based on CMAKE_C_COMPILER)
+
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
@@ -6,11 +11,20 @@ set(TOOLCHAIN_PREFIX arm-none-eabi-)
 # Without that flag CMake is not able to pass test compilation check
 set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
 
+# Get full path to compiler
+execute_process(
+        COMMAND which ${TOOLCHAIN_PREFIX}gcc
+        OUTPUT_VARIABLE CMAKE_C_COMPILER
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+# Get compiler directory
 execute_process(
         COMMAND dirname ${CMAKE_C_COMPILER}
         OUTPUT_VARIABLE ARM_TOOLCHAIN_DIR
         OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 
 set(CMAKE_OBJCOPY ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}objcopy CACHE INTERNAL "objcopy tool")
--- utils/cmake/utils.cmake
+++ utils/cmake/utils.cmake
@@ -24,13 +24,15 @@ function(firmware_size target)
     add_custom_command(TARGET ${target} POST_BUILD
             COMMAND ${CMAKE_SIZE_UTIL} -B
             "${CMAKE_CURRENT_BINARY_DIR}/${target}${CMAKE_EXECUTABLE_SUFFIX}"
+            COMMENT "Firmware size details:"
             )
 endfunction()
 
-# Add a command to generate firmare in a provided format
+# Add a command to generate firmware in a provided format
 function(generate_object target suffix type)
     add_custom_command(TARGET ${target} POST_BUILD
             COMMAND ${CMAKE_OBJCOPY} -O${type}
             "${CMAKE_CURRENT_BINARY_DIR}/${target}${CMAKE_EXECUTABLE_SUFFIX}" "${CMAKE_CURRENT_BINARY_DIR}/${target}${suffix}"
+            COMMENT "Building ${target}${suffix}"
             )
 endfunction()
