4e5d5413712e988882cfe8ee94efb09dbb3da633
Pete Dietl <pdietl@users.noreply.github.com>
2021-09-29 11:13:25 +0200

    Initial Orb board and app to bring up 12v power rail

--- src/main.c
+++ src/main.c
@@ -1,51 +1,23 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr.h>
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
-
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
-
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
-#else
-/* A build error here means your board isn't set up to blink an LED. */
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0	""
-#define PIN	0
-#define FLAGS	0
-#endif
+#include <drivers/regulator.h>
 
 void main(void)
 {
-	const struct device *dev;
-	bool led_is_on = true;
-	int ret;
+    printk("Hello, world!\n");
+    printk("This is the orb\n");
 
-	dev = device_get_binding(LED0);
-	if (dev == NULL) {
-		return;
-	}
+    const struct device *vbat_sw_regulator = DEVICE_DT_GET(DT_PATH(vbat_sw));
+    const struct device *supply_12v = DEVICE_DT_GET(DT_PATH(supply_12v));
 
-	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
-	if (ret < 0) {
-		return;
-	}
+    if (!device_is_ready(vbat_sw_regulator) || !(device_is_ready(supply_12v))) {
+        printk("12V supply not ready!\n");
+        return;
+    }
 
-	while (1) {
-		gpio_pin_set(dev, PIN, (int)led_is_on);
-		led_is_on = !led_is_on;
-		k_msleep(SLEEP_TIME_MS);
-	}
+    regulator_enable(vbat_sw_regulator, NULL);
+    k_msleep(100);
+    regulator_enable(supply_12v, NULL);
 }
