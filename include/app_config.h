22d9790ae047409cc45bfea5fa9ff7cfafa82906
Pete Dietl <pdietl@users.noreply.github.com>
2021-10-08 10:17:26 +0200

    Add front unit RGB strip code

--- include/app_config.h
+++ include/app_config.h
@@ -8,11 +8,15 @@
 // Threads priorities
 
 // work queue                           CONFIG_SYSTEM_WORKQUEUE_PRIORITY = -1
-#define THREAD_PRIORITY_CAN_RX          5
-#define THREAD_PRIORITY_PROCESS_TX_MSG  7
-#define THREAD_PRIORITY_TEST_SEND_CAN   9
+#define THREAD_PRIORITY_CAN_RX 5
+#define THREAD_PRIORITY_PROCESS_TX_MSG 7
+#define THREAD_PRIORITY_TEST_SEND_CAN 9
 // main thread priority                 10 // set as
 // logging thread priority              14
+
+// Front unit RGB LEDs
+#define THREAD_PRIORITY_FRONT_UNIT_RGB_LEDS 4
+#define THREAD_STACK_SIZE_FRONT_UNIT_RGB_LEDS 256
 
 #ifndef CONFIG_CAN_ADDRESS_MCU
 #define CONFIG_CAN_ADDRESS_MCU 0x01
@@ -22,5 +26,4 @@
 #define CONFIG_CAN_ADDRESS_JETSON 0x80
 #endif
 
-
-#endif //ORB_FIRMWARE_APP_MAIN_INCLUDE_APP_CONFIG_H
+#endif // ORB_FIRMWARE_APP_MAIN_INCLUDE_APP_CONFIG_H
---
+++ src/distributor_leds/distributor_leds.c
@@ -0,0 +1,11 @@
+#include <device.h>
+#include <drivers/led_strip.h>
+#include <drivers/pwm.h>
+#include <zephyr.h>
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(distributor_leds);
+
+#include "distributor_leds.h"
+
+int do_distributor_rgb_leds(void) { return 0; }
