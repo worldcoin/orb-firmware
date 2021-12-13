99bf21c23eb3789ae9a894b2067f1faba904928f
Pete Dietl <pdietl@users.noreply.github.com>
2021-12-13 13:16:35 +0100

    Liquid lens temperature reading

--- main_board/app/src/temperature/temperature.c
+++ main_board/app/src/temperature/temperature.c
@@ -9,9 +9,18 @@
 #include <zephyr.h>
 LOG_MODULE_REGISTER(temperature);
 
-const struct device *front_unit_temp_sensor =
-    DEVICE_DT_GET(DT_NODELABEL(front_unit_tmp_sensor));
-const struct device *mcu_internal_sensor = DEVICE_DT_GET(DT_PATH(stm_tmp));
+struct sensor_and_channel {
+    const struct device *sensor;
+    enum sensor_channel channel;
+};
+
+const struct sensor_and_channel sensors_and_channels[] = {
+    {.sensor = DEVICE_DT_GET(DT_NODELABEL(front_unit_tmp_sensor)),
+     .channel = SENSOR_CHAN_AMBIENT_TEMP},
+    {.sensor = DEVICE_DT_GET(DT_PATH(stm_tmp)),
+     .channel = SENSOR_CHAN_DIE_TEMP},
+    {.sensor = DEVICE_DT_GET(DT_NODELABEL(liquid_lens_tmp_sensor)),
+     .channel = SENSOR_CHAN_AMBIENT_TEMP}};
 
 static K_THREAD_STACK_DEFINE(stack_area, THREAD_STACK_SIZE_TEMPERATURE);
 static struct k_thread thread_data;
@@ -48,35 +57,21 @@ get_ambient_temperature(const struct device *dev, int32_t *temp,
                 ret);
         return 1;
     }
+    LOG_DBG("%s: %uC", dev->name, temp_value.val1);
     *temp = temp_value.val1;
 
     return 0;
 }
 
 static void
-sample_front_unit_temperature(void)
+sample_and_report_temperature(
+    const struct sensor_and_channel *sensor_and_channel)
 {
     int ret;
     int32_t temp;
 
-    ret = get_ambient_temperature(front_unit_temp_sensor, &temp,
-                                  SENSOR_CHAN_AMBIENT_TEMP);
-    if (!ret) {
-        msg.message.m_message.payload.temperature.source =
-            Temperature_TemperatureSource_FRONT_UNIT;
-        msg.message.m_message.payload.temperature.temperature_c = temp;
-        messaging_push_tx(&msg);
-    }
-}
-
-static void
-sample_mcu_internal_temperature(void)
-{
-    int ret;
-    int32_t temp;
-
-    ret = get_ambient_temperature(mcu_internal_sensor, &temp,
-                                  SENSOR_CHAN_DIE_TEMP);
+    ret = get_ambient_temperature(sensor_and_channel->sensor, &temp,
+                                  sensor_and_channel->channel);
     if (!ret) {
         msg.message.m_message.payload.temperature.source =
             Temperature_TemperatureSource_MAIN_MCU;
@@ -95,22 +90,31 @@ thread_entry_point(void *a, void *b, void *c)
     for (;;) {
         k_sleep(global_sample_period);
 
-        sample_front_unit_temperature();
-        sample_mcu_internal_temperature();
+        for (size_t i = 0; i < ARRAY_SIZE(sensors_and_channels); ++i) {
+            if (device_is_ready(sensors_and_channels[i].sensor)) {
+                sample_and_report_temperature(&sensors_and_channels[i]);
+            }
+        }
+    }
+}
+
+static void
+check_ready(void)
+{
+    for (size_t i = 0; i < ARRAY_SIZE(sensors_and_channels); ++i) {
+        if (!device_is_ready(sensors_and_channels[i].sensor)) {
+            LOG_ERR("Could not initialize temperature sensor '%s'",
+                    sensors_and_channels[i].sensor->name);
+        } else {
+            LOG_INF("initialized %s", sensors_and_channels[i].sensor->name);
+        }
     }
 }
 
 void
 temperature_init(void)
 {
-    if (!device_is_ready(front_unit_temp_sensor)) {
-        LOG_ERR("Could not initialize front unit temp sensor!");
-        return;
-    }
-    if (!device_is_ready(mcu_internal_sensor)) {
-        LOG_ERR("Could not initialize mcu internal temp sensor!");
-        return;
-    }
+    check_ready();
 
     global_sample_period = K_SECONDS(1);
 
