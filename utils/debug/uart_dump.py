82738e2f10f3d53bc480d03f26022f35d7878b25
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2022-02-16 15:37:22 +0100

    Print beautiful logs to better notice important messages

--- utils/debug/uart_dump.py
+++ utils/debug/uart_dump.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python
+import datetime
 import getopt
-from serial import Serial
+import re
 import sys
+
+from serial import Serial
+
+
+class colors:
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4'
+    SLOW = '\033[5'
+    FAST = '\033[6'
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    BLINK = '\033[5m'
+    VERBOSE = GREEN
+    INFO = WHITE
+    ERROR = RED
+    FATAL = RED
+    WARNING = YELLOW
+
 
 
 def main(argv):
@@ -29,6 +55,10 @@ def main(argv):
     except getopt.GetoptError:
         print(help)
 
+    # find "[00000000]" or [00:00:00.000,000]
+    timestamp_re = re.compile(r'(\[[0-9]{8}\])')
+    formatted_timestamp_re = re.compile(r'(\[([0-9]{2}.){3}[0-9]{3}\,[0-9]{3}\])')
+
     ser = Serial(port, 115200)
     print("🎧 Listening UART (8N1 {}) on {}".format(baud_rate, port))
 
@@ -42,6 +72,26 @@ def main(argv):
             to_print = line.decode('utf-8').rstrip()
         except Exception as err:
             print("ERROR: {}".format(err))
+
+        text_color = colors.END
+        if to_print.__contains__("<err>"):
+            text_color = colors.RED
+        elif to_print.__contains__("<wrn>"):
+            text_color = colors.YELLOW
+        elif to_print.__contains__("<inf>"):
+            text_color = colors.GREEN
+        elif to_print.__contains__("<dbg>"):
+            text_color = colors.END
+
+        now = datetime.datetime.now()
+
+        with_colored_remote_time = to_print
+        if timestamp_re.search(to_print):
+            with_colored_remote_time = timestamp_re.sub(colors.BLUE + '\\1' + text_color, to_print)
+        if formatted_timestamp_re.search(to_print):
+            with_colored_remote_time = formatted_timestamp_re.sub(colors.BLUE + '\\1' + text_color, to_print)
+        local_time = f"{now.isoformat(' ')[:23]}" + colors.END
+        to_print = local_time + " " + with_colored_remote_time + colors.END
 
         print(to_print)
 
