7ad0ab20b5e272e8f5a0dfc2a727d79a5f51a123
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2022-02-25 14:54:44 +0100

    Add logging over CAN. Messages are sent by chunk if larger than 51 bytes, and separated by CRLF "\r\n" characters. Fixes

---
+++ lib/log_backend_can/include/log_backend_can.h
@@ -0,0 +1,8 @@
+//
+// Copyright (c) 2022 Tools for Humanity. All rights reserved.
+//
+
+#ifndef ORB_MCU_MAIN_APP_LOG_BACKEND_CAN_H
+#define ORB_MCU_MAIN_APP_LOG_BACKEND_CAN_H
+
+#endif // ORB_MCU_MAIN_APP_LOG_BACKEND_CAN_H
