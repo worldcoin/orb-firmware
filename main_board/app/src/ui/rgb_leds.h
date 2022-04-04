87a2fe3f0ea10c779da513bad2ef5fdc7bd52a77
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2022-04-04 10:59:44 +0200

    Add static check to make sure we are passing an LED array.

--- main_board/app/src/ui/rgb_leds.h
+++ main_board/app/src/ui/rgb_leds.h
@@ -1,26 +1,31 @@
 #ifndef ORB_MCU_MAIN_APP_RGB_LEDS_H
 #define ORB_MCU_MAIN_APP_RGB_LEDS_H
 
+#include <compilers.h>
+
+#define ARRAY_SIZE_ASSERT(arr)                                                 \
+    (sizeof(arr) / sizeof((arr)[0]) + __must_be_array(arr))
+
 #define RGB_LEDS_OFF(leds) memset(leds, 0, sizeof leds)
 
 #define RGB_LEDS_WHITE(leds, brightness) memset(leds, brightness, sizeof leds)
 
 #define RGB_LEDS_RED(leds, brightness)                                         \
-    for (size_t i = 0; i < ARRAY_SIZE(leds); ++i) {                            \
+    for (size_t i = 0; i < ARRAY_SIZE_ASSERT(leds); ++i) {                     \
         leds[i].r = brightness;                                                \
         leds[i].g = 0;                                                         \
         leds[i].b = 0;                                                         \
     }
 
 #define RGB_LEDS_GREEN(leds, brightness)                                       \
-    for (size_t i = 0; i < ARRAY_SIZE(leds); ++i) {                            \
+    for (size_t i = 0; i < ARRAY_SIZE_ASSERT(leds); ++i) {                     \
         leds[i].r = 0;                                                         \
         leds[i].g = brightness;                                                \
         leds[i].b = 0;                                                         \
     }
 
 #define RGB_LEDS_BLUE(leds, brightness)                                        \
-    for (size_t i = 0; i < ARRAY_SIZE(leds); ++i) {                            \
+    for (size_t i = 0; i < ARRAY_SIZE_ASSERT(leds); ++i) {                     \
         leds[i].r = 0;                                                         \
         leds[i].g = 0;                                                         \
         leds[i].b = brightness;                                                \
