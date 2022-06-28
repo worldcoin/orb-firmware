d932fa608c8f2cb589176dc2b904ccd52344c3ea
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2022-06-28 11:44:05 +0200

    Do not shut down with long-press on button from main MCU but wait for Orb-core to send instructions

--- main_board/app/src/button/button.c
+++ main_board/app/src/button/button.c
@@ -15,8 +15,6 @@ static const struct gpio_dt_spec button_spec =
     GPIO_DT_SPEC_GET_OR(POWER_BUTTON_NODE, gpios, {0});
 static struct gpio_callback button_cb_data;
 static bool is_init = false;
-static struct k_work_delayable dwork_detect_shutdown;
-#define BUTTON_PRESS_SHUTDOWN_DURATION_MS 5000
 
 /// Interrupt context
 static void
@@ -34,33 +32,7 @@ button_event_handler(const struct device *dev, struct gpio_callback *cb,
                 .message.m_message.which_payload = McuToJetson_power_button_tag,
                 .message.m_message.payload.power_button.pressed = ret};
             can_messaging_async_tx(&button_state);
-
-            if (ret == 1) {
-                // button pressed
-                ret =
-                    k_work_schedule(&dwork_detect_shutdown,
-                                    K_MSEC(BUTTON_PRESS_SHUTDOWN_DURATION_MS));
-                if (ret >= 0) {
-                    return;
-                }
-            } else if (ret == 0) {
-                // button released, cancel scheduled work
-                k_work_cancel_delayable(&dwork_detect_shutdown);
-            }
         }
-    }
-}
-
-static void
-button_press_delayed_worker(struct k_work *work)
-{
-    UNUSED_PARAMETER(work);
-
-    // 5 second press detected, init shutdown sequence
-    if (gpio_pin_get_dt(&button_spec) == 1) {
-        LOG_INF("Button pressed %us, shutting down",
-                BUTTON_PRESS_SHUTDOWN_DURATION_MS);
-        power_reset(1);
     }
 }
 
@@ -125,8 +97,6 @@ button_init(void)
         ASSERT_SOFT(err_code);
         return RET_ERROR_INTERNAL;
     }
-
-    k_work_init_delayable(&dwork_detect_shutdown, button_press_delayed_worker);
 
     LOG_INF("Power button initialized");
     is_init = true;
