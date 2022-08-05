146397fd2507593958f6d1c504cf2ca17fddfb7b
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2022-08-05 09:52:21 +0200

    Add soft assert to print error if getting message from queue fails

--- lib/can_messaging/canbus_tx_isotp.c
+++ lib/can_messaging/canbus_tx_isotp.c
@@ -68,6 +68,7 @@ process_tx_messages_thread()
         int ret = k_msgq_get(&isotp_tx_msg_queue, &new, K_FOREVER);
         if (ret != 0) {
             // error
+            ASSERT_SOFT(ret);
             continue;
         }
 
