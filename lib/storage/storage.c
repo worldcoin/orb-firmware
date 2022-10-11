18fe7bb8dd065ea92075d3c50bffcd802ce25f0d
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2022-10-11 15:20:14 +0200

    storage: protect usage with semaphore
    Signed-off-by: Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>

--- lib/storage/storage.c
+++ lib/storage/storage.c
@@ -17,6 +17,8 @@ static struct storage_area_s storage_area = {0};
 #define UNUSED_UINT16       0xFFFF
 #define MINIMUM_EMPTY_SPACE 512 //!< empty space to keep before erasing flash
 
+K_SEM_DEFINE(sem_storage, 1, 1);
+
 static int
 init_area(const struct flash_area *fa)
 {
@@ -101,9 +103,13 @@ reset_area(const struct flash_area *fa)
 int
 storage_peek(char *record, size_t *size)
 {
+    int ret = RET_SUCCESS;
+    k_sem_take(&sem_storage, K_FOREVER);
+
     // verify storage is not empty
     if (storage_area.rd_idx == storage_area.wr_idx) {
-        return RET_ERROR_NOT_FOUND;
+        ret = RET_ERROR_NOT_FOUND;
+        goto exit;
     }
 
     // verify record is valid with correct CRC
@@ -113,7 +119,8 @@ storage_peek(char *record, size_t *size)
                      (const uint8_t *)storage_area.rd_idx +
                          sizeof(storage_header_t),
                      storage_area.rd_idx->record_size))) {
-        return RET_ERROR_INVALID_STATE;
+        ret = RET_ERROR_INVALID_STATE;
+        goto exit;
     }
 
     memcpy(record,
@@ -122,16 +129,22 @@ storage_peek(char *record, size_t *size)
 
     *size = storage_area.rd_idx->record_size;
 
-    return RET_SUCCESS;
+exit:
+    k_sem_give(&sem_storage);
+
+    return ret;
 }
 
 int
 storage_free(void)
 {
-    int ret;
+    int ret = RET_SUCCESS;
+
+    k_sem_take(&sem_storage, K_FOREVER);
 
     if (storage_area.rd_idx->magic_state != RECORD_VALID) {
-        return RET_ERROR_NOT_FOUND;
+        ret = RET_ERROR_NOT_FOUND;
+        goto exit;
     }
 
     // keep a copy of the freed record size before invalidating it
@@ -144,7 +157,8 @@ storage_free(void)
                            (const void *)&header, sizeof(header));
     if (ret) {
         LOG_ERR("Unable to invalidate record: %d", ret);
-        return RET_ERROR_INTERNAL;
+        ret = RET_ERROR_INTERNAL;
+        goto exit;
     }
 
     // push read index
@@ -162,21 +176,28 @@ storage_free(void)
         }
     }
 
-    return RET_SUCCESS;
+exit:
+    k_sem_give(&sem_storage);
+
+    return ret;
 }
 
 int
 storage_push(char *record, size_t size)
 {
-    int ret;
+    int ret = RET_SUCCESS;
+
+    k_sem_take(&sem_storage, K_FOREVER);
 
     if (((uint32_t)storage_area.wr_idx + size) >
         (storage_area.fa->fa_off + storage_area.fa->fa_size)) {
-        return RET_ERROR_NO_MEM;
+        ret = RET_ERROR_NO_MEM;
+        goto exit;
     }
 
     if (size % FLASH_WRITE_BLOCK_SIZE != 0) {
-        return RET_ERROR_INVALID_PARAM;
+        ret = RET_ERROR_INVALID_PARAM;
+        goto exit;
     }
 
     // fill header
@@ -193,7 +214,8 @@ storage_push(char *record, size_t size)
                            (const void *)record, size);
     if (ret) {
         reset_area(storage_area.fa);
-        return RET_ERROR_INTERNAL;
+        ret = RET_ERROR_INTERNAL;
+        goto exit;
     }
 
     // verify flash is correctly written
@@ -203,7 +225,9 @@ storage_push(char *record, size_t size)
                     size)) {
         LOG_ERR("Invalid CRC16 read after record has been written");
         reset_area(storage_area.fa);
-        return RET_ERROR_INVALID_STATE;
+
+        ret = RET_ERROR_INVALID_STATE;
+        goto exit;
     }
 
     // write header
@@ -212,14 +236,19 @@ storage_push(char *record, size_t size)
                            (const void *)&header, sizeof(header));
     if (ret) {
         reset_area(storage_area.fa);
-        return RET_ERROR_INTERNAL;
+
+        ret = RET_ERROR_INTERNAL;
+        goto exit;
     }
 
     // push write index
     storage_area.wr_idx = (storage_header_t *)((size_t)storage_area.wr_idx +
                                                (sizeof(header) + size));
 
-    return RET_SUCCESS;
+exit:
+    k_sem_give(&sem_storage);
+
+    return ret;
 }
 
 int
@@ -227,22 +256,29 @@ storage_init(void)
 {
     int ret;
 
+    k_sem_take(&sem_storage, K_FOREVER);
+
     ret = flash_area_open(FLASH_AREA_ID(storage), &storage_area.fa);
     if (ret) {
         LOG_ERR("Unable to open flash area: %d", ret);
-        return RET_ERROR_NOT_INITIALIZED;
+        ret = RET_ERROR_NOT_INITIALIZED;
+        goto exit;
     }
 
     ret = init_area(storage_area.fa);
     if (ret == RET_ERROR_INVALID_STATE) {
         LOG_WRN("Unable to find valid records, erasing area");
         flash_area_erase(storage_area.fa, 0, storage_area.fa->fa_size);
+        ret = RET_SUCCESS;
     }
 
     LOG_INF("Storage initialized: rd: 0x%x, wr: 0x%x",
             (uint32_t)storage_area.rd_idx, (uint32_t)storage_area.wr_idx);
 
-    return RET_SUCCESS;
+exit:
+    k_sem_give(&sem_storage);
+
+    return ret;
 }
 
 #if CONFIG_ORB_LIB_STORAGE_TESTS
