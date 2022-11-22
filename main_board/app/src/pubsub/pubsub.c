7318f217b9a46b9e90add5bc1f55f869089e49c7
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2022-11-22 10:41:07 +0100

    pubsub: bytes to wrap McuMessage asserted at compile time.
    Signed-off-by: Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>

--- main_board/app/src/pubsub/pubsub.c
+++ main_board/app/src/pubsub/pubsub.c
@@ -34,6 +34,17 @@ static K_THREAD_STACK_DEFINE(pub_stored_stack_area,
                              THREAD_STACK_SIZE_PUB_STORED);
 static struct k_thread pub_stored_thread_data;
 
+#if JetsonToMcu_size >= JetsonToSec_size &&                                    \
+    JetsonToMcu_size >= McuToJetson_size &&                                    \
+    JetsonToMcu_size >= SecToJetson_size
+#define MCU_MESSAGE_ENCODED_WRAPPER_SIZE (McuMessage_size - JetsonToMcu_size)
+#else
+// A payload is larger than JetsonToMcu which does not allow to find the number
+// of bytes needed to wrap the payload into an McuMessage. Please use another
+// field to calculate the number of bytes needed to wrap the payload.
+#error Unable to calculate bytes needed to wrap a payload into an McuMessage.
+#endif
+
 /// Structure holding a message to store when sending isn't enabled
 /// `data` contains an `McuToJetson` into an `McuMessage`.
 /// The McuMessage is encoded (serialized).
@@ -42,7 +53,7 @@ static struct k_thread pub_stored_thread_data;
 /// `McuMessage`
 struct pub_entry_s {
     uint32_t destination;
-    uint8_t data[McuToJetson_size + 5];
+    uint8_t data[McuToJetson_size + MCU_MESSAGE_ENCODED_WRAPPER_SIZE];
 };
 
 enum sub_priority_e {
