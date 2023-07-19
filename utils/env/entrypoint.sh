3a3d35bc54336a93c4eba4e30f81dd9e071dcbf7
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2023-07-19 18:47:24 +0200

    env: simpler entrypoint logic

--- utils/env/entrypoint.sh
+++ utils/env/entrypoint.sh
@@ -1,6 +1,4 @@
-#!/bin/bash --login
-# The --login should ensure the bash configuration is loaded (e.g. ~/.bash_profile)
-# allowing us to invoke conda as `conda init bash` writes init scripts to ~/.bash_profile
+#!/bin/bash
 
 set -euo pipefail
 conda activate worldcoin
