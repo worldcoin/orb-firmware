1cd9f8ee1234d764095699d8108282a4e5886ab9
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2023-10-06 10:21:14 -0700

    env: fix environment

--- utils/env/entrypoint.sh
+++ utils/env/entrypoint.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+# load env based on initial shell
+if [ -n "${ZSH_NAME:-}" ]; then
+    . "$HOME"/.zshrc
+elif [ -n "${BASH:-}" ]; then
+    . "$HOME"/.bashrc
+fi
 
 set -euo pipefail
 conda activate worldcoin
