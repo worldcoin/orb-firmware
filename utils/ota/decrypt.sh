3b53f82c0608ac4caa1adf8ae2050bb83cc9d605
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2024-01-29 16:31:56 +0100

    dfu: simpler script to decrypt keys
    not naming the key files that are originally present
    in the repo but removed from its open-source
    counterpart

--- utils/ota/decrypt.sh
+++ utils/ota/decrypt.sh
@@ -6,13 +6,13 @@ BASEDIR=$(dirname $0)
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$FW_KEYS_PASSPHRASE" \
---output ${BASEDIR}/enc-ec256-priv.pem ${BASEDIR}/enc-ec256-priv.pem.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$FW_KEYS_PASSPHRASE" \
---output ${BASEDIR}/enc-ec256-pub.pem ${BASEDIR}/enc-ec256-pub.pem.gpg
-
-gpg --quiet --batch --yes --decrypt --passphrase="$FW_KEYS_PASSPHRASE" \
---output ${BASEDIR}/root-ec-p256.pem ${BASEDIR}/root-ec-p256.pem.gpg
-
-echo "Successfully decrypted keys"
+# for all files ending in .pem.gpg
+for file in ${BASEDIR}/*.pem.gpg; do
+  # remove the .gpg extension
+  filename="${file%.*}"
+  # decrypt with the passphrase
+  echo "Decrypting $file to $filename"
+  gpg --quiet --batch --yes --decrypt --passphrase="$FW_KEYS_PASSPHRASE" \
+  --output $filename $file
+done
