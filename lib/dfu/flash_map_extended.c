5d2517c4cf698bce7ef44153c4e8cb7f141fee2a
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2024-11-21 17:20:45 +0100

    chore(zephyr): build app
    app is building
    still many changes to be made
    
    Signed-off-by: Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>

--- lib/dfu/flash_map_extended.c
+++ lib/dfu/flash_map_extended.c
@@ -79,11 +79,11 @@ flash_area_id_from_image_slot(int slot)
 int
 flash_area_id_to_multi_image_slot(int image_index, int area_id)
 {
-    if (area_id == FLASH_AREA_IMAGE_PRIMARY(image_index)) {
+    if ((uint32_t)area_id == FLASH_AREA_IMAGE_PRIMARY(image_index)) {
         return 0;
     }
 #if !defined(CONFIG_SINGLE_APPLICATION_SLOT)
-    if (area_id == FLASH_AREA_IMAGE_SECONDARY(image_index)) {
+    if ((uint32_t)area_id == FLASH_AREA_IMAGE_SECONDARY(image_index)) {
         return 1;
     }
 #endif
