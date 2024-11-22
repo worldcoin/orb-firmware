1dcf688b217b6f66bc8a1d957def2ff878e9e317
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2024-11-22 10:59:39 +0100

    chore(zephyr): temporary remove cone support
    support will be provided when needed
    
    Signed-off-by: Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>

--- main_board/src/ui/ui.c
+++ main_board/src/ui/ui.c
@@ -36,18 +36,6 @@ ui_init(void)
 #if defined(CONFIG_BOARD_DIAMOND_MAIN)
     err_code = white_leds_init();
     ASSERT_SOFT(err_code);
-
-#if defined(CONFIG_DT_HAS_DIAMOND_CONE_ENABLED)
-    BUILD_ASSERT(CONFIG_SC18IS606_INIT_PRIO > CONFIG_I2C_INIT_PRIO_INST_2,
-                 "I2C4 must be configured before using it by SC16IS606.");
-    BUILD_ASSERT(CONFIG_LED_STRIP_INIT_PRIORITY >
-                     CONFIG_SC18IS606_CHANNEL_INIT_PRIO,
-                 "SC18IS606 must be configured before using it by the LED "
-                 "strip driver.");
-
-    cone_present = (cone_leds_init() == RET_SUCCESS);
-    ui_cone_present_send(CONFIG_CAN_ADDRESS_DEFAULT_REMOTE);
-#endif
 #endif
 
     return RET_SUCCESS;
