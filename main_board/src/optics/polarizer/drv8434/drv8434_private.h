648d12ea32aa124921f026f2e74e4156f6df8158
Srikar <srikar.chintapalli@toolsforhumanity.com>
2024-11-24 22:05:12 -0800

    feat(motor driver): drv8434 privates
    drv8434 SPI transaction layer
    
    Signed-off-by: Srikar Chintapalli <srikar.chintapalli@toolsforhumanity.com>

---
+++ main_board/src/optics/polarizer/drv8434/drv8434_private.h
@@ -0,0 +1,54 @@
+/******************************************************************************
+ * @file drv8434_private.h
+ * @brief Private header file for Texas Instruments DRV8434 stepper motor driver
+ *
+ * This file declares the register read/write functions, and the lowest
+ * level functions used to interface with the chip via the given SPI driver
+ *
+ *
+ * @note All register addresses and bit definitions are based on
+ *       DRV8434 datasheet SLOSE70 – DECEMBER 2020
+ *
+ * @author Srikar Chintapalli
+ *
+ ******************************************************************************/
+
+#include "errors.h"
+
+#include "drv8434_datatypes.h"
+
+/**
+ * @brief DRV8434 Register read function
+ *
+ * @param address Register Address to Read
+ * @param drv8434 instance Description of second parameter
+ *
+ * @return Description of return value
+ *
+ * @details
+ * This functon is responsible for a reading a register
+ * on the chip using a BLOCKING SPI transfer function
+ *
+ */
+
+ret_code_t
+drv8434_private_reg_read(uint8_t address, DRV8434_Instance_t *instance);
+
+/**
+ * @brief DRV8434 Register write function
+ *
+ * @param address Register Address to Write
+ * @param data    Data to write
+ * @param drv8434 instance Description of second parameter
+ *
+ * @return Description of return value
+ *
+ * @details
+ * This functon is responsible for a writing a register
+ * on the chip using a BLOCKING SPI transfer function
+ *
+ */
+
+ret_code_t
+drv8434_private_reg_write(uint8_t address, uint8_t data,
+                          DRV8434_Instance_t *instance);
