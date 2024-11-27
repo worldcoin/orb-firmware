48381fb8d0a0ec057570cf3da1421e28041be675
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2024-11-27 14:24:17 +0100

    fix(clion): fix entrypoint.sh
    ensure we start with a clean PATH
    
    Signed-off-by: Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>

--- utils/env/entrypoint.sh
+++ utils/env/entrypoint.sh
@@ -1,8 +1,14 @@
 # Load the miniconda environment
 # Can be used as `environment file` in CLion when setting up the toolchain
 
+# reset PATH variable
+export PATH=$(getconf PATH)
+
 # load env based on initial shell
 if [ -n "${ZSH_NAME:-}" ]; then
+    # see `man zsh`
+    . "$HOME"/.zshenv
+    . /etc/zshrc
     . "$HOME"/.zshrc
 elif [ -n "${BASH:-}" ]; then
     . "$HOME"/.bashrc
