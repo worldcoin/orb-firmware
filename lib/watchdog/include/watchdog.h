e81796490fc46a67fb321eaf852daf5c2b02d202
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2024-11-29 11:42:25 +0100

    chore(watchdog): init right after device driver
    use sys_init to initialize watchdog
    callback cannot be set dynamically anymore, but weakly defined instead
    so that it can be overridden by user at compile time.
    
    Signed-off-by: Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>

--- lib/watchdog/include/watchdog.h
+++ lib/watchdog/include/watchdog.h
@@ -1,6 +1,23 @@
 #pragma once
 
 #include <stdbool.h>
+
+/**
+ * @brief Perform checks to determine if watchdog should be fed
+ *
+ * Weakly defined function in `watchdog.c` that can be overridden by the user
+ */
+bool
+watchdog_perform_checks(void);
+
+/**
+ * @brief Stop feeding the watchdog
+ */
+void
+watchdog_stop_feed(void);
+
+#if !defined(CONFIG_ORB_LIB_WATCHDOG_SYS_INIT)
+
 /**
  * Setup watchdog & spawn low-priority thread to reload the watchdog
  *
@@ -14,3 +31,5 @@
  */
 int
 watchdog_init(bool (*callback)(void));
+
+#endif
