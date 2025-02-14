c21af96cf684aa9377733119880f765fdbf14f40
Srikar Chintapalli <srikar.chintapalli@toolsforhumanity.com>
2025-02-13 22:13:48 -0800

    feat(polarizer): initial implementation
    initial polarizer wheel homing
    drv8434s driver
    interrupt-based pulse/step count
    set motor direction
    
    Signed-off-by: Srikar Chintapalli <srikar.chintapalli@toolsforhumanity.com>

---
+++ main_board/src/optics/polarizer_wheel/polarizer_wheel.h
@@ -0,0 +1,62 @@
+/******************************************************************************
+ * @file polarizer_wheel.h
+ * @brief Header file for Polarizer wheel functions
+ *
+ * This file declares the interface layer between the polarizer wheel
+ *application and the motor of choice
+ *
+ *
+ * @author Srikar Chintapalli
+ *
+ ******************************************************************************/
+
+#include "drv8434/drv8434.h"
+#include "polarizer_wheel_datatypes.h"
+
+/**
+ * @brief Initialize the Polarizer Wheel runtime context
+ *
+ * @return Indicates successful initialization of runtime context
+ *
+ * @details This functon is responsible for initializing the Polarizer wheel
+ *          run-time context.
+ */
+
+ret_code_t
+polarizer_wheel_init(void);
+
+/**
+ * @brief Configure the Polarizer Wheel Motor and surrounding peripherals
+ *
+ * @return Indicates successful configuration of polarizer wheel
+ *
+ * @details This functon is responsible for configuring the Polarizer wheel
+ *          operating parameters and surrounding peripherals.
+ */
+
+ret_code_t
+polarizer_wheel_configure(void);
+
+/**
+ * @brief Get current position of the Polarizer Wheel
+ *
+ *
+ * @return Returns positional enum
+ *
+ *
+ */
+
+Polarizer_Wheel_Position_t
+polarizer_wheel_get_position(void);
+
+/**
+ * @brief Set current position of the Polarizer Wheel
+ *
+ *
+ * @return Returns positional enum
+ *
+ *
+ */
+
+ret_code_t
+polarizer_wheel_set_position(Polarizer_Wheel_Position_t position);
