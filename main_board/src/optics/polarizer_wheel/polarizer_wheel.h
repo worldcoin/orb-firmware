db4264c98f3356e89dbae034e6a0b748ba4a985d
Srikar Chintapalli <srikar.chintapalli@toolsforhumanity.com>
2025-02-18 17:18:18 -0800

    feat(polarizer): it builds
    it builds
    
    Signed-off-by: Srikar Chintapalli <srikar.chintapalli@toolsforhumanity.com>

--- main_board/src/optics/polarizer_wheel/polarizer_wheel.h
+++ main_board/src/optics/polarizer_wheel/polarizer_wheel.h
@@ -2,17 +2,16 @@
  * @file polarizer_wheel.h
  * @brief Header file for Polarizer wheel functions
  *
- * This file declares the interface layer between the polarizer wheel application
- * and the motor of choice
+ * This file declares the interface layer between the polarizer wheel
+ *application and the motor of choice
  *
  *
  * @author Srikar Chintapalli
  *
  ******************************************************************************/
 
-#include "polarizer_wheel_datatypes.h"
 #include "drv8434/drv8434.h"
-
+#include "polarizer_wheel_datatypes.h"
 
 /**
  * @brief Initialize the Polarizer Wheel runtime context
@@ -26,14 +25,13 @@
 ret_code_t
 polarizer_wheel_init(void);
 
-
 /**
  * @brief Configure the Polarizer Wheel Motor and surrounding peripherals
  *
  * @return Indicates successful configuration of polarizer wheel
- * 
+ *
  * @details This functon is responsible for configuring the Polarizer wheel
- *          operating parameters and surrounding peripherals.  
+ *          operating parameters and surrounding peripherals.
  */
 
 ret_code_t
@@ -50,3 +48,15 @@ polarizer_wheel_configure(void);
 
 Polarizer_Wheel_Position_t
 polarizer_wheel_get_position(void);
+
+/**
+ * @brief Set current position of the Polarizer Wheel
+ *
+ *
+ * @return Returns positional enum
+ *
+ *
+ */
+
+ret_code_t
+polarizer_wheel_set_position(Polarizer_Wheel_Position_t position);
