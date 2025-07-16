3e1fa2ba6943f4078c9040693aeb63dd6c985621
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2025-07-16 17:28:42 +0200

    feat(self-test): fan & tachometer
    implement test and report fan & tachometer system status
    
    Signed-off-by: Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>

--- main_board/src/temperature/fan/fan_tests.c
+++ main_board/src/temperature/fan/fan_tests.c
@@ -1,10 +1,62 @@
 #include "fan.h"
 #include "fan_tach.h"
-#include "system/version/version.h"
-#include <zephyr/ztest.h>
+#include <orb_state.h>
+#include <zephyr/kernel.h>
 
 #include "orb_logs.h"
 LOG_MODULE_REGISTER(fan_test, LOG_LEVEL_INF);
+
+ORB_STATE_REGISTER(fan_tach);
+
+void
+fan_tach_self_test(void)
+{
+    // initial speed
+    uint16_t initial_speed_value = fan_get_speed_setting();
+    uint32_t main_speed = fan_tach_get_main_speed();
+#if defined(CONFIG_BOARD_PEARL_MAIN)
+    uint32_t aux_speed = fan_tach_get_aux_speed();
+#endif
+    // increase speed to double the initial value
+    fan_set_speed_by_value(initial_speed_value * 2);
+
+    k_msleep(1000);
+    uint32_t main_speed_second = fan_tach_get_main_speed();
+#if defined(CONFIG_BOARD_PEARL_MAIN)
+    uint32_t aux_speed_second = fan_tach_get_aux_speed();
+#endif
+
+    if (main_speed != UINT32_MAX && main_speed_second != UINT32_MAX) {
+        if (main_speed_second > main_speed) {
+            ORB_STATE_SET_CURRENT(RET_SUCCESS);
+        } else {
+            ORB_STATE_SET_CURRENT(RET_ERROR_ASSERT_FAILS,
+                                  "speed didn't increase");
+        }
+    } else if (main_speed == 0 || main_speed_second == 0) {
+        ORB_STATE_SET_CURRENT(RET_ERROR_ASSERT_FAILS, "fan not running");
+    }
+
+#if defined(CONFIG_BOARD_PEARL_MAIN)
+    if (aux_speed != UINT32_MAX && aux_speed_second != UINT32_MAX) {
+        if (aux_speed_second > aux_speed) {
+            ORB_STATE_SET_CURRENT(RET_SUCCESS);
+        } else {
+            ORB_STATE_SET_CURRENT(RET_ERROR_ASSERT_FAILS,
+                                  "aux speed didn't increase");
+        }
+    } else if (aux_speed == 0 || aux_speed_second == 0) {
+        ORB_STATE_SET_CURRENT(RET_ERROR_ASSERT_FAILS, "fan not running");
+    }
+#endif
+
+    // reset speed to initial value
+    fan_set_speed_by_value(initial_speed_value);
+}
+
+#if defined(CONFIG_ZTEST)
+#include "system/version/version.h"
+#include <zephyr/ztest.h>
 
 ZTEST(hil, test_fan_set_speed)
 {
@@ -54,3 +106,5 @@ ZTEST(hil, test_fan_tachometer)
         zassert_false(true, "No fan was spinning");
     }
 }
+
+#endif // CONFIG_ZTEST
