52be5028eae6324d268bd256b8d758f96edde4eb
Filip Kubicz <filip.kubicz@toolsforhumanity.com>
2025-08-22 20:34:15 +0200

    add strobe/flash as input and set up high prio interrupt

--- main_board/include/app_config.h
+++ main_board/include/app_config.h
@@ -131,3 +131,5 @@
 #define CAMERA_SWEEP_INTERRUPT_PRIO     10
 #define LED_940NM_GLOBAL_INTERRUPT_PRIO 11
 #define LED_850NM_GLOBAL_INTERRUPT_PRIO 11
+// External RGB-IR camera STROBE (FLASH) EXTI interrupt priority (lower is higher)
+#define RGB_IR_STROBE_INTERRUPT_PRIO     1
